a91975d053066e9f06d2d42c0dd63da10ecc6eea
WaceraMercyThird <waceraaamercy@gmail.com>
2022-04-01 07:24:07 +0300

    add mab libs game

---
+++ mad_libs_geme/main.py
@@ -0,0 +1,20 @@
+
+# # mad libs game
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input ("Enter a celebrity: ")
+
+print("Flowers are " +color)
+print(plural_noun + "are green")
+print("I love" + celebrity)
+
+# List
+friends = ["Kelvin", "Karen", "Jim", "Karo", "mwangi"
+]
+print(friends[2])
+print(friends[-1])
+print(friends[2:5])
+friends[2]= "Ken"
+
+
+
